06012f7b99523dfca61fcd388b091092f7bf5db9
Thierry Laforge <21laforgth@gmail.com>
2021-11-18 22:46:28 -0700

    Test commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+Starting point for our project. Took some time to play around with pandas and made script to index a sample set of data.
+Not sure if you want to share code through here, GitHub, LiveShare or some other code sharing thingy. Let me know if
+this works or if you have a better idea. Anything is better than Google Drive in my opinion lmao.
---
+++ main.py
@@ -0,0 +1,22 @@
+# TODO: Obtain datasets to work with. Put into datasets folder.
+# TODO: Import data sets in order to read them.
+# TODO: Create search engine that uses keywords. (Can be done using lists?)
+# TODO: Figure out if using pandas can replace the 3 numpy arrays as per the requirements.
+# TODO: Figure out how the user input can manipulate the data in the csv (Pandas can write to cells easily I think.)
+# TODO: Once finding the data, display to user in nice way. (Data table using matplotlib or using CLI table.)
+# TODO: Once all this shite is done make other shit like the pdf and prep the demo
+# TODO: Return to monke
+# TODO: Don't eat shit
+# Run 'python -m pip install -r requirements.txt' after activating virtual environment.
+# (Top line really just install pandas for TAs. Still not 100% sure if we can use it but Marasco said we could so idk)
+import pandas
+
+
+# Imports the csv file and sets the first column as row indices
+# (Might have to change this next line if the data is formatted a different way)
+data = pandas.read_csv(r"datasets\Population_Data.csv", index_col=0)
+print(data)
+
+# Able to now access items by DataFrame.loc[row, column]
+peepee = data.loc["Vietnam", "2018 Pop"]
+print(peepee)
